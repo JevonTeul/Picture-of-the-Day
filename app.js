fba9bef49f824dc582546050ab6b57a24f5a9a81
Jevon Teul <2022157000@ub.edu.bz>
2025-04-07 04:04:18 -0600

    Completed

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.get('/get-content', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
